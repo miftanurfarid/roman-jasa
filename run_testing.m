7b1aa6966888f5ffdc0a697283e791168fa122a0
miftanurfarid <miftanurfarid@gmail.com>
2017-02-06 20:27:06 +0700

    change load to audioread

--- run_testing.m
+++ run_testing.m
@@ -1,69 +1,84 @@
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%TESTING EXAMPLE - TWO-SOURCE CONFIGURATION%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function run_testing(namefile_configuration, inputfile_target, inputfile_noise1,inputfile_noise2)
 
- F1=441; %sampling frequency of the HRTF database is 44.1 kHZ
- F2=160; %sampling frequency of the original signals in the test set is 16 kHz
+F1 = 441; %sampling frequency of the HRTF database is 44.1 kHZ
   
- output_dir='test';
- mkdir(output_dir);
- 'load configuration file'
-  load(namefile_configuration)
+output_dir='test';
+mkdir(output_dir);
+disp('load configuration file');
+load(namefile_configuration);
 
-'simulate binaural signal'
-order_conf=length(Az);
+disp('simulate binaural signal');
+order_conf = length(Az);
 
-if order_conf==2 
-    if nargin<3
-    'eror syntax: namefile_configuration input_target input_noise'
+if order_conf == 2
+    if nargin < 3
+    disp('error syntax: namefile_configuration input_target input_noise')
     end
     
-name=strcat(inputfile_target);
-x1=load(name);x1=resample(x1,F1,F2);
-
-name=strcat(inputfile_noise1);   
-x2=load(name);x2=resample(x2,F1,F2);
-
-N=min(length(x1),length(x2));
-
-X=[reshape(x1(1:N),1,N);reshape(x2(1:N),1,N)];
+    name = strcat(inputfile_target);
+    [x1,fs] = audioread(name);
+    x1 = resample(x1,F1,fs);
+    
+    name = strcat(inputfile_noise1);
+    [x2,fs] = audioread(name);
+    x2 = resample(x2,F1,fs);
+    
+    N = min(length(x1),length(x2));
+    
+    X = [reshape(x1(1:N),1,N);reshape(x2(1:N),1,N)];
 end
 
 
-if order_conf==3
-    if nargin<4
-    'eror syntax: namefile_configuration input_target input_noise1 input_noise2'
+if order_conf == 3
+    if nargin < 4
+        disp('error syntax: namefile_configuration input_target input_noise1 input_noise2')
     end
     
-name=strcat(inputfile_target);
-x1=load(name);x1=resample(x1,F1,F2);
+    name = strcat(inputfile_target);
+    [x1,fs] = audioread(name);
+    x1 = resample(x1,F1,fs);
+    
+    name = strcat(inputfile_noise1);
+    [x2,fs] = audioread(name);
+    x2 = resample(x2,F1,fs);
 
-name=strcat(inputfile_noise1);   
-x2=load(name);x2=resample(x2,F1,F2);
-
-name=strcat(inputfile_noise2);   
-x3=load(name);x3=resample(x3,F1,F2);
-
-N=min([length(x1),length(x2),length(x3)]);
-
-X=[reshape(x1(1:N),1,N);reshape(x2(1:N),1,N);reshape(x2(1:N),1,N)];
+    name = strcat(inputfile_noise1);
+    [x3,fs] = audioread(name);
+    x3 = resample(x3,F1,fs);
+    
+    N = min([length(x1),length(x2),length(x3)]);
+    
+    X = [reshape(x1(1:N),1,N);reshape(x2(1:N),1,N);reshape(x2(1:N),1,N)];
 end
 
 simulateSignal(X,Az,output_dir);
 
-'feature extraction'
+disp('feature extraction');
 
-!extractFeatures test
+if ispc == 1
+    !extractFeatures test
+end
 
-'mask estimation'
+if isunix == 1
+    !./extractFeatures test
+end
+
+disp('mask estimation');
 
 estimateMask(output_dir,D,Region);
 
-'resynthesis'
+disp('resynthesis');
 
-!resynthesize
+if ispc == 1
+    !resynthesize
+end
 
+if isunix == 1
+    !./resynthesize
+end
 
+end
 %%%%%%%%%%END%%%%%%%%%%%
